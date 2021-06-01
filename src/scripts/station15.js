97a3f1e95982309c98105efcf78951a9de536201
github MinamisoutH <kanou373@gmail.com>
2021-06-02 00:55:31 +0900

    Station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -4,7 +4,17 @@ async function getData() {
     return await result
 }
 
-function test() {
-  return
+async function test() {
+  return new Promise (function(resolve) {
+    setTimeout(function() {
+      resolve('test')
+    }, 3000);
+  }).then(function(value) {
+    console.log(value);
+    const test = [
+      {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affilication: 'TechTrain', is_student: false},
+      {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affilication: 'HogeHoge大学', is_student: true}
+    ];
+    return test.map(test => test.full_name)
+  });
 }
-
