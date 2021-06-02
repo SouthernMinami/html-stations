30629945ce538f9f949b58658d8caaec1f03b32e
github MinamisoutH <kanou373@gmail.com>
2021-06-02 11:46:59 +0900

    Station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -10,11 +10,11 @@ async function test() {
       resolve('test')
     }, 3000);
   }).then(function(value) {
-    console.log(value);
     const test = [
       {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affilication: 'TechTrain', is_student: false},
       {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affilication: 'HogeHoge大学', is_student: true}
     ];
-    return test.map(test => test.full_name)
+    console.log(value);
+    return test
   });
 }
